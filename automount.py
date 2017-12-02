2a15f54a5f61da9d962f5f9d70fc84a418d241c3
BradHeff <ph3onix83@gmail.com>
2017-12-03 08:28:38 +1030

    Created the script

--- automount.py
+++ automount.py
@@ -2,16 +2,30 @@
 
 import os
 
-class mountdrive():
+
+class Mountdrive():
     def __init__(self):
-        print("Mounting /dev/sdb1 - /media/tv")
-        os.system("mount /dev/sdb1 /media/tv")
-        print("Mounting /dev/sdc1 - /media/movie")
-        os.system("mount /dev/sdc1 /media/movie")
-        print("Mounting /dev/sdd1 - /media/external")
-        os.system("mount /dev/sdd1 /media/external")
-        print("Mounting /dev/sde1 - /media/SAMSUNG")
-        os.system("mount /dev/sde1 /media/SAMSUNG")
+        try:
+            print("Mounting /dev/sdb1 - /media/tv")
+            os.system("mount /dev/sdb1 /media/tv")
+        except:
+            print("No drive /dev/sdb1")
+        try:
+            print("Mounting /dev/sdc1 - /media/movie")
+            os.system("mount /dev/sdc1 /media/movie")
+        except:
+            print("No drive /dev/sdc1")
+        try:
+            print("Mounting /dev/sdd1 - /media/external")
+            os.system("mount /dev/sdd1 /media/external")
+        except:
+            print("No drive /dev/sdd1")
+        try:
+            print("Mounting /dev/sde1 - /media/SAMSUNG")
+            os.system("mount /dev/sde1 /media/SAMSUNG")
+        except:
+            print("No drive /dev/sdr1")
+
 
 if __name__ == '__main__':
-    mountdrive()
+    Mountdrive()
