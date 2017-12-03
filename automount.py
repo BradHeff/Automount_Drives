3c0d550c5292af723d4e1e6303ad0301f9328444
BradHeff <ph3onix83@gmail.com>
2017-12-03 15:18:35 +1030

    Created the script

--- automount.py
+++ automount.py
@@ -5,26 +5,43 @@ import os
 
 class Mountdrive():
     def __init__(self):
+        print("================================================")
+        print("==============Auto Mount Activated==============")
+        print("================================================")
         try:
             print("Mounting /dev/sdb1 - /media/tv")
             os.system("mount /dev/sdb1 /media/tv")
+            self.seperator("/dev/sdb1")
         except:
-            print("No drive /dev/sdb1")
+            self.errortext("/dev/sdb1")
         try:
             print("Mounting /dev/sdc1 - /media/movie")
             os.system("mount /dev/sdc1 /media/movie")
+            self.seperator("/dev/sdc1")
         except:
-            print("No drive /dev/sdc1")
+            self.errortext("/dev/sdc1")
         try:
-            print("Mounting /dev/sdd1 - /media/external")
-            os.system("mount /dev/sdd1 /media/external")
+            print("Mounting /dev/sdd1 - /media/SAMSUNG")
+            os.system("mount /dev/sdd1 /media/SAMSUNG")
+            self.seperator("/dev/sdd1")
         except:
-            print("No drive /dev/sdd1")
+            self.errortext("/dev/sdd1")
         try:
-            print("Mounting /dev/sde1 - /media/SAMSUNG")
-            os.system("mount /dev/sde1 /media/SAMSUNG")
+            print("Mounting /dev/sde1 - /media/external")
+            os.system("mount /dev/sde1 /media/external")
+            self.seperator("/dev/sde1")
         except:
-            print("No drive /dev/sdr1")
+            self.errortext("/dev/sde1")
+
+    @staticmethod
+    def seperator(text):
+        print(text + " mount Success")
+        print("==================================")
+
+    @staticmethod
+    def errortext(text):
+        print("No mount point " + text)
+        print("==================================")
 
 
 if __name__ == '__main__':
